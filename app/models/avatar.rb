71ef1cf6e6c92c48c200c400ecc1a6ee2a89c36b
Avalokitesvara <kannon@zrcadlo.me>
2014-04-13 21:27:50 -0600

    no eye, ear, nose, tongue, body or mind; no form, sound, smell, taste, touch or mind object;

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,3 +1,4 @@
 class Avatar < ActiveRecord::Base
   has_attached_file :visage, styles: {medium: "300x300>", thumb: "100x100>"}
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
